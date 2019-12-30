6330e84ed81d4a1fb88af194de3e7f2e3f47f1c5
Marche <marche_871@hotmail.com>
2019-12-30 00:24:41 -0300

    paralax + routes ipdates

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,31 +1,9 @@
 <template>
   <v-app id="inspire">
-
-    <v-app-bar app clipped-left color="red">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title thin display-3>Flay</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-          <v-list-item link v-for="item in this.actionItems" v-bind:key="item.name" :to=item.url>
-              <v-list-item-action>
-                <v-icon>{{item.icon}}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>{{item.name}}</v-list-item-title>
-              </v-list-item-content>
-          </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-content fluid>
-      <router-view/>
-    </v-content>
+       <router-view></router-view>>
 
-    <v-footer app>
-      <span>&copy; 2019</span>
-    </v-footer>
+    </v-content>
   </v-app>
 </template>
 
@@ -33,6 +11,7 @@
 import "reflect-metadata";
 import { Component, Vue, Inject } from "vue-property-decorator";
 import Vuetify from "vuetify";
+import Home from "./views/Home.vue";
 
 Vue.use(Vuetify);
 
---
+++ front/src/views/Template.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app id="inspire">
+
+    <v-app-bar app clipped-left color="red">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title thin display-3>Flay</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+          <v-list-item link v-for="item in this.actionItems" v-bind:key="item.name" :to=item.url>
+              <v-list-item-action>
+                <v-icon>{{item.icon}}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{item.name}}</v-list-item-title>
+              </v-list-item-content>
+          </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content fluid>
+      <router-view/>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import { Component, Vue, Inject } from "vue-property-decorator";
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
+
+@Component
+export default class Template extends Vue {
+ 
+  drawer!: boolean;
+  actionItems!: { name: string; url: string; icon: string }[];
+  constructor() {
+    super();
+    this.$vuetify.theme.dark = true;
+    this.drawer = false;
+    this.load();
+  }
+
+  private async load() {
+    this.actionItems = [
+        { name: "Dashboard", url: "/dashboard", icon: "mdi-view-dashboard" },
+        { name: "Edifices", url: "/edifices", icon: "mdi-office-building"}
+      ];
+  }
+}
+</script>
+
+<style>
+a {
+  color: inherit;
+  text-decoration: inherit;
+}
+</style>
