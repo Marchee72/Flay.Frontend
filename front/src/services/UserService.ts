8e232d82abcddcd18cbad8cbce3cb260455091cb
Marche <marche_871@hotmail.com>
2020-05-11 17:52:27 -0300

    user data

--- front/src/services/UserService.ts
+++ front/src/services/UserService.ts
@@ -1,63 +1,62 @@
 import { injectable } from "inversify-props";
 import { Access } from "@/models/Access";
 import authHeader from "@/helpers/auth-header";
-import {URL} from "@/helpers/http-helpers";
+import { URL } from "@/helpers/http-helpers";
 import { IUserService } from "@/interfaces/IUserService";
 import { User } from "@/models/User";
 
 @injectable()
 export class UserService implements IUserService {
+  async getAllUsers(): Promise<User[]> {
+    const promise = await fetch(URL("user", "getAllUsers"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "GET"
+    });
+    return (await promise.json()) as User[];
+  }
 
-    async getAllUsers(): Promise<User[]> {
-        const promise = await fetch(URL("user", "getAllUsers"), {
-        headers: authHeader(),
-        credentials: "same-origin",
-        method: "GET"
-        });
-        return (await promise.json()) as User[];
-    }
+  async getUserPermissions(): Promise<Access[]> {
+    const promise = await fetch(URL("user", "getPermisions"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "GET"
+    });
+    return (await promise.json()) as Access[];
+  }
 
-    async getUserPermissions(): Promise<Access[]> {
-        const promise = await fetch(URL("user", "getPermisions"), {
-            headers: authHeader(),
-            credentials: "same-origin",
-            method: "GET"
-          });
-          return (await promise.json()) as Access[];
-    }
+  updateProfilePicture(image: string): void {
+    fetch(URL("user", "updateImage"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "POST",
+      body: JSON.stringify(image)
+    });
+  }
 
-    updateProfilePicture(image: string): void {
-        fetch(URL("user", "updateImage"), {
-            headers: authHeader(),
-            credentials: "same-origin",
-            method: "POST",
-            body: JSON.stringify(image)
-        });
-    }
+  async getProfilePicture(): Promise<string> {
+    const promise = await fetch(URL("user", "getProfilePicture"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "GET"
+    });
+    return (await promise.json()) as string;
+  }
 
-    async getProfilePicture(): Promise<string> {
-        const promise = await fetch(URL("user", "getProfilePicture"), {
-            headers: authHeader(),
-            credentials: "same-origin",
-            method: "GET",
-        });
-        return (await promise.json()) as string;
-    }
+  removeProfilePicture(): void {
+    fetch(URL("user", "removeProfilePicture"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "GET"
+    });
+  }
 
-    removeProfilePicture(): void {
-        fetch(URL("user", "removeProfilePicture"), {
-            headers: authHeader(),
-            credentials: "same-origin",
-            method: "GET",
-        });
-    }
-
-    async getUserInformation(): Promise<User> {
-        const promise = await fetch(URL("user", "getUser"), {
-            headers: authHeader(),
-            credentials: "same-origin",
-            method: "GET",
-        });
-        return (await promise.json()) as User;
-    }
+  async getUserInformation(): Promise<User> {
+    const promise = await fetch(URL("user", "getUser"), {
+      headers: authHeader(),
+      credentials: "same-origin",
+      method: "GET"
+    });
+    return (await promise.json()) as User;
+  }
 }
--- front/src/views/user/Data.vue
+++ front/src/views/user/Data.vue
@@ -1,10 +1,23 @@
 <template>
   <v-container>
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-card>
-        <v-card-title>Informacion basica</v-card-title>
-      </v-card>
-    </v-form>
+    <v-card>
+      <v-card-title>Informacion basica</v-card-title>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-col cols="6" center>
+          <v-row cols="12">
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Nombre" outlined></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Apellido" outlined></v-text-field>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field label="E-mail" outlined></v-text-field>
+        </v-col>
+      </v-form>
+    </v-card>
   </v-container>
 </template>
 
