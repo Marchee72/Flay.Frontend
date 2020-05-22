b335a92e18c876f34db51efef4e27a168ee4c971
Franco <francof_nob@hotmail.com>
2020-05-22 17:23:42 -0300

    Alta edificio

--- front/src/models/Building.ts
+++ front/src/models/Building.ts
@@ -4,12 +4,14 @@ export class Building {
   id!: string;
   buildingName!: string;
   streetName!: string;
-  streetnumber!: string;
+  streetNumber!: string;
   floors!: number;
   administrator!: User;
-  constructor(buildingName: string, streetName: string, floors: number) {
+
+  constructor(buildingName: string, streetName: string, streetNumber: string,floors: number) {
     this.buildingName = buildingName;
     this.streetName = streetName;
+    this.streetNumber = streetNumber;
     this.floors = floors;
   }
 }
